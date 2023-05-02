57919c69605c9effe27b31ef703a2cf79e573872
Maksimenka Matvei <70953270+MatveiBG@users.noreply.github.com>
2023-05-02 14:11:06 +0200

    more pretty + more better

--- Game.py
+++ Game.py
@@ -5,24 +5,24 @@ from Map import *
 import copy
 
 class Game(object):
-    equipments = { 0: [ Equipment("potion","!"), Equipment("gold","o") ], 1: [ Equipment("sword"), Equipment("bow") ], 2: [ Equipment("chainmail") ] }
-    monsters = { 0: [ Creature("Goblin",4), Creature("Bat",2,"W") ], 1: [ Creature("Ork",6,strength=2), Creature("Blob",10) ], 5: [ Creature("Dragon",20,strength=3) ] }
+    equipments = { 0: [ Equipment("potion","!"), Equipment("gold","o") ],
+                  1: [ Equipment("sword"), Equipment("bow") ],
+                  2: [ Equipment("chainmail") ] }
+    monsters = { 0: [ Creature("Goblin",4), Creature("Bat",2,"W") ],
+                1: [ Creature("Ork",6,strength=2), Creature("Blob",10) ],
+                5: [ Creature("Dragon",20,strength=3) ] }
+    
     def __init__(self,hero=None,level=1):
-        if hero is None:
-            self._hero=Hero()
-        else:
-            self._hero=hero
-        self._level=level
-        self._floor=None
-        self._message=[]
+        self._hero = hero if hero else Hero()
+        self._level = level
+        self._floor = None
+        self._message = []
         
     def buildFloor(self):
-        self._floor=Map(hero=self._hero)
-        
-        
+        self._floor = Map(hero=self._hero)
+           
     def addMessage(self,msg):
         self._message.append(msg)
-        
         
     def readMessages(self):
         lecture=""
@@ -46,16 +46,13 @@ class Game(object):
         
         return elementChoisiMultiple
         
-        
-        
     def randEquipment(self):
         return self.randElement(Game.equipments)
     
     
     def randMonster(self):
         return self.randElement(Game.monsters)
-    
-    
+
 def theGame(game = Game()):
     return game
         
